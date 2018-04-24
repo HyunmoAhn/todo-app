6615b870728a72ccba1dc78cf0011307244a2e54
hyunmoahn <mos_dev@naver.com>
2018-04-25 01:31:39 +0900

    Make example component and container about counter

--- ReactJS/src/index.jsx
+++ ReactJS/src/index.jsx
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import 'node_modules/todomvc-app-css/index.css';
 import 'node_modules/todomvc-common/base.css';
 import Todo from 'components/Todo';
+import CounterContainer from 'containers/CounterContainer';
+import { Provider } from 'react-redux';
+
+import store from './store';
 
 ReactDOM.render(
-  <Todo />,
+  <Provider store={store}>
+    <div>
+      <Todo />
+      <CounterContainer />
+    </div>
+  </Provider>,
   document.getElementById('root'),
 );
