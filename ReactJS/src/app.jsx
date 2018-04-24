34ab71addf83d72327a68ca47a861be0a9f5aa38
hyunmoahn <mos_dev@naver.com>
2018-04-25 01:43:16 +0900

    Attach react-hot-loader

---
+++ ReactJS/src/app.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import Todo from 'components/Todo';
+import Counter from 'containers/CounterContainer';
+import { hot } from 'react-hot-loader';
+import store from './store';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div>
+        <Todo />
+        <Counter />
+      </div>
+    </Provider>
+  );
+}
+
+export default hot(module)(App);
--- ReactJS/src/index.jsx
+++ ReactJS/src/index.jsx
@@ -2,18 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'node_modules/todomvc-app-css/index.css';
 import 'node_modules/todomvc-common/base.css';
-import Todo from 'components/Todo';
-import CounterContainer from 'containers/CounterContainer';
-import { Provider } from 'react-redux';
-
-import store from './store';
+import App from './app';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <div>
-      <Todo />
-      <CounterContainer />
-    </div>
-  </Provider>,
+  <App />,
   document.getElementById('root'),
 );
