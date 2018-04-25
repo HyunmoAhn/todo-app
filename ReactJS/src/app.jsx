ae86318ec918d6541aeae1f7fbfe386b3517baeb
hyunmoahn <mos_dev@naver.com>
2018-04-25 14:21:35 +0900

    Attach append todo list in component, container

--- ReactJS/src/app.jsx
+++ ReactJS/src/app.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import Todo from 'components/Todo';
+import Todo from 'containers/TodoContainer';
 import { hot } from 'react-hot-loader';
 import store from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <div>
-        <Todo />
-      </div>
+      <Todo />
     </Provider>
   );
 }
