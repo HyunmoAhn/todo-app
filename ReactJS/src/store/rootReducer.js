730e045c4cc4d5c23ce16d36713def539cf7fbe6
hyunmoahn <mos_dev@naver.com>
2018-04-26 16:47:40 +0900

    Delete sample redux code about counter

--- ReactJS/src/store/rootReducer.js
+++ ReactJS/src/store/rootReducer.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
-import counter from './counter/reducers';
 import todo from './todo/reducers';
 
 const rootReducer = combineReducers({
-  counter,
   todo,
 });
 
