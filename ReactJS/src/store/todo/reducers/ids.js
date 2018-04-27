6ad6ebe0b231fb13a204ea4efafe132aa6fa4400
hyunmoahn <mos_dev@naver.com>
2018-04-28 00:34:25 +0900

    Apply set localStorage

--- ReactJS/src/store/todo/reducers/ids.js
+++ ReactJS/src/store/todo/reducers/ids.js
@@ -1,6 +1,6 @@
 import * as TYPES from 'store/actionTypes';
 
-const initialState = [];
+const initialState = JSON.parse(localStorage.getItem('ids')) || [];
 
 function idsReducer(state = initialState, actions) {
   switch (actions.type) {
@@ -8,12 +8,16 @@ function idsReducer(state = initialState, actions) {
       const nextState = state.slice();
 
       nextState.push(actions.payload.id);
+
+      localStorage.setItem('ids', JSON.stringify(nextState));
       return nextState;
     }
     case TYPES.TODO_DELETE_LIST_ITEM: {
       const { id } = actions.payload;
+      const nextState = state.filter(item => item !== id);
 
-      return state.filter(item => item !== id);
+      localStorage.setItem('ids', JSON.stringify(nextState));
+      return nextState;
     }
     case TYPES.TODO_CLEAR_COMPLETED_ITEMS: {
       const nextState = state.slice();
@@ -21,6 +25,7 @@ function idsReducer(state = initialState, actions) {
 
       items.forEach(item => nextState.splice(nextState.indexOf(item), 1));
 
+      localStorage.setItem('ids', JSON.stringify(nextState));
       return nextState;
     }
     default: {
